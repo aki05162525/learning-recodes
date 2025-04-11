3ea9935b4376a88f5622fceeacbec26375c53059
aki05162525 <akihiro05162525@gmail.com>
2025-04-11 09:55:07 +0900

    cssスタイルの勉強

--- Udemy/React-beginner/react-guide-material/07_styling_component/src/040_css_in_js/start/Example.jsx
+++ Udemy/React-beginner/react-guide-material/07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -1,4 +1,30 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+console.dir(styled);
+const StyledButton = styled.button`
+  margin: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+  background: ${(props) => (props.isSelected ? "#4caf50" : "#f44336")};
+
+  @media (max-width: 600px) {
+    border-radius: 0;
+  }
+`;
+
+const OrangeButton = styled(StyledButton)`
+  background: orange;
+
+  :hover {
+    color: red;
+    opacity: 0.5;
+  }
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,6 +33,12 @@ const Example = () => {
 
   return (
     <>
+      <StyledButton isSelected={isSelected} onClick={clickHandler}>
+        ボタン
+      </StyledButton>
+      <OrangeButton isSelected={isSelected} onClick={clickHandler}>
+        ボタン
+      </OrangeButton>
       <button
         className={`btn ${isSelected ? "selected" : ""}`}
         onClick={clickHandler}
