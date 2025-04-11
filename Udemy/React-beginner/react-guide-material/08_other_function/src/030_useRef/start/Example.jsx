fc6711d65bf4b614be88916c5d498d890c70b114
aki05162525 <akihiro05162525@gmail.com>
2025-04-11 11:08:37 +0900

    DOM操作の勉強

--- Udemy/React-beginner/react-guide-material/08_other_function/src/030_useRef/start/Example.jsx
+++ Udemy/React-beginner/react-guide-material/08_other_function/src/030_useRef/start/Example.jsx
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef();
+
+  // console.log(inputRef);
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
-        インプット要素をフォーカスする
+      <input
+        type="text"
+        ref={inputRef}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick>インプット要素をフォーカスする</button>
+    </div>
+  );
+};
+
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+
+  return (
+    <div>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button
+        onClick={() => {
+          if (playing) {
+            videoRef.current.pause();
+          } else {
+            videoRef.current.play();
+          }
+          setPlaying((prev) => !prev);
+        }}
+      >
+        {playing ? "Stop" : "Play"}
       </button>
     </div>
   );
@@ -18,6 +49,7 @@ const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
